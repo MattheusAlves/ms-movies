97f8640a67af26b2f569fde33bc0a2eb4e5cea2a
MattheusAlves <matheus_alves0101@outlook.com>
2021-02-11 11:09:29 -0300

    typescript improvements

--- src/pages/api/series/popular.ts
+++ src/pages/api/series/popular.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-export default async function category(
+export default async function popular(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
